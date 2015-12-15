a0f183fdc62ad275c05e612de3a83add50758d09
Philip Kimmey <philip@rover.com>
2015-12-15 10:41:32 -0800

    Remove tce-load step since rsync is now in boot2docker

--- dockermachine.go
+++ dockermachine.go
@@ -7,26 +7,6 @@ import (
 	"os/exec"
 )
 
-func Provision(machineName string, verbose bool) {
-	c := []string{
-		// install and run rsync daemon
-		`tce-load -wi rsync attr acl`,
-
-		// disable boot2dockers builtin vboxfs
-		// TODO bad idea, because you then can't use vboxfs anymore
-		// `sudo umount /Users || /bin/true`,
-	}
-
-	for _, v := range c {
-		out, err := RunSSHCommand(machineName, v, verbose)
-		if err != nil {
-			fmt.Println(err)
-			fmt.Printf("%s\n", out)
-			os.Exit(1)
-		}
-	}
-}
-
 func RunSSHCommand(machineName, command string, verbose bool) (out []byte, err error) {
 	if verbose {
 		fmt.Println(`docker-machine ssh ` + machineName + ` '` + command + `'`)
